f7ae9684c70a3d692dc4132274627ed668aabaae
Rodrigo Augusto Amaral <s2it_ramaral@uolinc.com>
2019-09-11 11:27:22 -0300

    ajuste validator

--- src/main/java/br/com/cast/avaliacao/component/InputValidator.java
+++ src/main/java/br/com/cast/avaliacao/component/InputValidator.java
@@ -1,23 +1,26 @@
 package br.com.cast.avaliacao.component;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+
 import org.springframework.context.MessageSource;
 import org.springframework.context.MessageSourceAware;
 import org.springframework.stereotype.Component;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Locale;
-import java.util.function.Supplier;
-import java.util.stream.Collectors;
 
 @Component
 public class InputValidator implements MessageSourceAware {
 
     private static final String DELIMITER = ", ";
     private static final Object[] DEFAULT_OBJECTS = {};
-    private List<String> invalidInputs = new ArrayList<>();
 
+    private List<String> invalidInputs = new ArrayList<>();
+    private Map<Supplier<Boolean>, String> mapInputs = new HashMap<>();
     private MessageSource messageSource;
 
     @Override
@@ -26,22 +29,24 @@ public class InputValidator implements MessageSourceAware {
     }
 
     public InputValidator check(final Supplier<Boolean> supplier, final String input) {
-        if (!supplier.get()) {
-            invalidInputs.add(input);
-        }
+        mapInputs.put(supplier, input);
         return this;
     }
 
     public Object[] getInvalidInputs() {
+        invalidInputs.clear();
+        mapInputs.keySet().forEach(supplierKey -> {
+            if (supplierKey.get()) {
+                invalidInputs.add(mapInputs.get(supplierKey));
+            }
+        });
         final String msg = invalidInputs.stream()
                 .map((codeStr) -> messageSource.getMessage(
                         codeStr,
-                       null,
+                        null,
                         Locale.getDefault()))
                 .collect(Collectors.joining(DELIMITER));
-        invalidInputs = new ArrayList<>();
         return msg.isEmpty() ? DEFAULT_OBJECTS : Collections.singletonList(msg).toArray();
     }
-
 
 }
