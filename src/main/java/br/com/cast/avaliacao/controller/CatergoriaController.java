f2927727125781c2b8e10134adbd1b08f29cc13b
Rodrigo Augusto Amaral <s2it_ramaral@uolinc.com>
2019-09-30 15:40:33 -0300

    ajuste imports

--- src/main/java/br/com/cast/avaliacao/controller/CatergoriaController.java
+++ src/main/java/br/com/cast/avaliacao/controller/CatergoriaController.java
@@ -1,20 +1,26 @@
 package br.com.cast.avaliacao.controller;
 
-import br.com.cast.avaliacao.model.CategoriaEntity;
-import br.com.cast.avaliacao.model.dto.CategoriaDTO;
-import br.com.cast.avaliacao.service.CategoriaService;
-import br.com.cast.avaliacao.util.Mapper;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.validation.Valid;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
+import br.com.cast.avaliacao.model.dto.CategoriaDTO;
+import br.com.cast.avaliacao.service.CategoriaService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @Controller
--- src/main/java/br/com/cast/avaliacao/controller/CursoController.java
+++ src/main/java/br/com/cast/avaliacao/controller/CursoController.java
@@ -1,28 +1,32 @@
 package br.com.cast.avaliacao.controller;
 
-import br.com.cast.avaliacao.model.CursoEntity;
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import br.com.cast.avaliacao.model.dto.CursoDTO;
 import br.com.cast.avaliacao.service.CursoService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
-import org.springframework.context.MessageSourceAware;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
-import java.util.Collections;
-import java.util.List;
-import java.util.Locale;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @Controller
 @RequestMapping("/curso")
 @Api(value = "Curso")
-public class CursoController extends BaseController  {
+public class CursoController extends BaseController {
 
     @Autowired
     private CursoService cursoService;
